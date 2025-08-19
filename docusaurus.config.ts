ab9d7a9585cb2b6434a636ec870f8d9ba05f32db
Aevarkan <aevarkan@gmail.com>
2025-08-20 00:25:55 +0800

    chore: deployment branch is named `pages`

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Aevarkan', // Usually your GitHub org/user name.
   projectName: 'Tutorials', // Usually your repo name.
+  deploymentBranch: 'pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
