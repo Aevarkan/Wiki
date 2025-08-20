e648b1f2b0ddbbbb42cd1040389eadcde6f00600
Aevarkan <aevarkan@gmail.com>
2025-08-20 21:56:09 +0800

    chore: add latex packages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,9 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -42,6 +45,8 @@ const config: Config = {
       'classic',
       {
         docs: {
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
           sidebarPath: './sidebars.ts',
           routeBasePath: "/",
           // Please change this to your repo.
@@ -55,6 +60,13 @@ const config: Config = {
         },
       } satisfies Preset.Options,
     ],
+  ],
+
+  stylesheets: [
+    {
+      href: '/katex/katex.min.css',
+      type: 'text/css',
+    }
   ],
 
   themeConfig: {
